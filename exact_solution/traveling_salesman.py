9c77008bfcaaa3aa0f2399477f14df5275777b3a
Dylan Dao <daodt@dukes.jmu.edu>
2025-11-17 10:40:59 -0500

    brute force tsp

--- exact_solution/traveling_salesman.py
+++ exact_solution/traveling_salesman.py
@@ -1,9 +1,40 @@
 # starts with complete graph
 # Hamiltonian cycle
 # John Henry Adams, Dylan Dao, Harry Williams
+"""Number of cities: 4
+City 1: 0 10 15 20
+City 2: 0 0 35 25
+City 3: 15 35 0 30
+City 4: 20 25 30 0
+"""
+import itertools
+
+
+def brute_force_tsp(dist):
+    n = len(dist)
+    best_cost = float("inf")
+    best_path = None
+
+    for perm in itertools.permutations(range(1, n)):
+        tour = (0,) + perm + (0,)
+        cost = sum(dist[tour[i]][tour[i+1]] for i in range(n))
+
+        if cost < best_cost:
+            best_cost = cost
+            best_path = tour
+
+    return best_cost, best_path
+
 
 def main():
-    pass
+    n = int(input("Number of cities: "))
+    dist = []
+    for i in range(1, n + 1):
+        row = list(map(int, input(f"City {i}: ").split()))
+        dist.append(row)
+
+    print(brute_force_tsp(dist))
+
 
 if __name__ == "__main__":
     main()
