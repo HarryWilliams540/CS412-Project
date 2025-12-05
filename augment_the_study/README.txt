3cb1208616391cac0518a95f80fae45365a64c1f
Dylan Dao <daodt@dukes.jmu.edu>
2025-12-04 21:42:45 -0500

    part e/f

--- augment_the_study/README.txt
+++ augment_the_study/README.txt
@@ -1,5 +1,5 @@
 run ./augment_copy.sh in the terminal to run tests in the test_cases folder comparing 
 the lower bound and approximation values
 
-run ./augment.sh in the terminal to run test in the aug_test_cases folder comparing
+run ./augment.sh in the terminal to run test in the tsp_graphs folder comparing
 the lower bound, approximation, and exact values
--- augment_the_study/augment.sh
+++ augment_the_study/augment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Loop over all test case files
-for file in aug_test_cases/*; do
+for file in tsp_graphs/*; do
     echo "Running $file..."
 
     # GNU date required for ms timing (macOS)
@@ -9,17 +9,26 @@ for file in aug_test_cases/*; do
     
     echo ">>> Running 1-tree lower bound"
     python3 augment.py < "$file"
+
+    end_time=$(gdate +%s%3N)
+    elapsed_time=$((end_time - start_time))
+    echo "Complete in ${elapsed_time} ms"
+    start_time=$(gdate +%s%3N)
     
     echo ">>> Running approximation solver"
     python3 ../approx_solution/cs412_tsp_approx.py < "$file"
+
+    end_time=$(gdate +%s%3N)
+    elapsed_time=$((end_time - start_time))
+    echo "Complete in ${elapsed_time} ms"
+    start_time=$(gdate +%s%3N)
 
     echo ">>> Running exact solver"
     python3 ../exact_solution/cs412_tsp_exact.py < "$file"
 
     end_time=$(gdate +%s%3N)
-
     elapsed_time=$((end_time - start_time))
-
     echo "Complete in ${elapsed_time} ms"
+
     echo "----------------------------"
 done
